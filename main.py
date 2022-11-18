b2df184262b49217264ff3f8bf2d017dafab3ab4
Raúl Coterillo <rcoterillo@bcamath.org>
2022-11-18 11:05:00 +0100

    unified lang

--- main.py
+++ main.py
@@ -21,15 +21,22 @@ class Storage():
 
    t0: float            # Extra access time per stack level above ground
 
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
    def __init__(self, t0: float) -> None:
-   
+      """ Class constructor. """
       self.A = []
       self.sn, self.tn, self.ln = [], [], []
       self.sk, self.ak, self.pk = [], [], []
       self.t0 = t0
 
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
    def __str__(self) -> str:
+      """ String representation of the class. """
       return json.dumps(self.A, sort_keys=True)
+
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
    def add_location(self, sn: float, tn: float, ln: bool = False) -> int:
       """ Add a location. """
@@ -40,6 +47,32 @@ class Storage():
       self.ln.append(ln)
       return idx
 
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+   def location_summary(self) -> pd.DataFrame:
+
+      """ Returns a DataFrame with storage summary information. """
+
+      items  = self.count_items()
+      stacks = self.count_stacks()
+      surf   = self.calc_surface()
+      acc    = self.calc_access()
+
+      df = pd.DataFrame(index=np.arange(len(self.A)))
+      df["items"] = items.sum(axis=1)
+      df["stacks"] = stacks.sum(axis=1)
+      df["superf_max"] = self.sn
+      df["superf_occ"] = surf.sum(axis=1)
+      df["superf_pct"] = surf.sum(axis=1)/self.sn*100
+      df["tmp_acc_base"] = self.tn
+      df["tmp_acc_items"] = acc.sum(axis=1)
+      df["balda"] = self.ln
+
+      return df
+
+
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
    def add_product(self, sk: float, ak: int, pk: bool) -> int:
       """ Add a product. """
       idx = len(self.sk)
@@ -47,6 +80,28 @@ class Storage():
       self.ak.append(ak)
       self.pk.append(pk)
       return idx
+
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+   
+   def product_summary(self) -> pd.DataFrame:
+
+      """ Returns a DataFrame with storage summary information. """
+
+      items  = self.count_items()
+      stacks = self.count_stacks()
+      surf   = self.calc_surface()
+
+      df = pd.DataFrame(index=np.arange(len(self.sk)))
+      df["total_items"] = items.sum(axis=0)
+      df["total_stcks"] = stacks.sum(axis=0)
+      df["surface_occ"] = self.sk
+      df["total_surf"] = surf.sum(axis=0)
+      df["stack_limit"] = self.ak
+      df["shelve_comp"] = self.pk
+
+      return df
+
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
    def add_item(self, n: int, k: int, s: int = None):
 
@@ -100,7 +155,7 @@ class Storage():
          elif s < len(self.A[n][k]):
          
             # Stack height check
-            if self.A[n][k][s] >= self.sk[k]:
+            if self.A[n][k][s] >= self.ak[k]:
                raise ValueError("Stack {s} already full!")
 
             # Add item to the stack
@@ -118,6 +173,8 @@ class Storage():
 
       return n, k, s
 
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
    def count_items(self) -> np.ndarray:
 
       """ Count stacks per location/product. """
@@ -129,6 +186,8 @@ class Storage():
             if k in self.A[n]:
                stacks[n,k] = np.array(self.A[n][k]).sum()
       return stacks
+
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
    def count_stacks(self, full: bool = None) -> np.ndarray:
 
@@ -157,6 +216,8 @@ class Storage():
                   stacks[n,k] = []
       return stacks
 
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
    def locate_stacks(self, full: bool = True) -> np.ndarray:
 
       """ Return location of full / not full stacks. """
@@ -179,6 +240,8 @@ class Storage():
                   stacks[n,k] = []
       return stacks
 
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
    def calc_surface(self) -> np.ndarray:
 
       """ Calculate used surface per location/product. """
@@ -191,6 +254,8 @@ class Storage():
                surf[n,k] = len(self.A[n][k])*self.sk[k]
       return surf
 
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
    def calc_location_surface(self, n: int) -> float:
 
       """ Calculate used surface at location n. """
@@ -200,6 +265,8 @@ class Storage():
          if k in self.A[n]:
             surf += len(self.A[n][k])*self.sk[k]
       return surf
+
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
    def calc_solution_surface(self, sol: np.ndarray, model: pd.DataFrame):
 
@@ -217,6 +284,8 @@ class Storage():
 
       return surf
 
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
    def calc_access(self) -> np.ndarray:
 
       """ Calculate access times per location/product. """
@@ -229,6 +298,8 @@ class Storage():
                            np.arange(len(self.A[n][k])).sum()*self.t0
       return acc
 
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
    def calc_solution_access(self, sol: np.ndarray, model: pd.DataFrame):
 
       """ Calculate extra access_time added by a solution. """
@@ -237,67 +308,25 @@ class Storage():
       df.insert(0, "amount", sol)
       df = df[df["amount"] > 0]
 
-      
       def _loc_time(x: np.array):
-         return self.tn[x]
+         return self.tn[x["location"]]
 
       def _stack_time(x: np.array):
-         return np.arange(x[1]).sum()*self.t0
+         return np.arange(x["height"], x["height"]+x["amount"]+1).sum()*self.t0
       
-      df["location"].apply(_loc_time)*df["height"]
-      df[["amount", "height"]].apply(_stack_time)
+      base_time = df[["location"]].apply(_loc_time, axis=1)*df["amount"]
+      stack_time = df[["height", "amount"]].apply(_stack_time, axis=1)
 
-      #np.apply_along_axis(_custom_func, df["height"].values)
-      
+      return (base_time + stack_time).sum()
 
-      # surf = 0
-      # for k, mk in df.iterrows():
-      #    surf += mk*self.sk[k]
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-      return df
-
-   def location_info(self, aggregate: bool = True) -> pd.DataFrame:
-
-      """ Returns a DataFrame with storage summary information. """
-
-      items  = self.count_items()
-      stacks = self.count_stacks()
-      surf   = self.calc_surface()
-      acc    = self.calc_access()
-
-      if aggregate:
-
-         df = pd.DataFrame(index=np.arange(len(self.A)))
-
-         df["items"] = items.sum(axis=1)
-         df["stacks"] = stacks.sum(axis=1)
-         df["superf_max"] = self.sn
-         df["superf_occ"] = surf.sum(axis=1)
-         df["superf_pct"] = surf.sum(axis=1)/self.sn*100
-         df["tmp_acc_base"] = acc.sum(axis=1)
-         df["tmp_acc_total"] = acc.sum(axis=1)
-         df["balda"] = self.ln
-
-      else:
-
-         index = pd.MultiIndex.from_product(
-               [np.arange(len(self.A)), np.arange(len(self.sk))], 
-               names=["loc", "prod"])
-         df = pd.DataFrame(index=index)
-
-         df["items"] = items.flatten()
-         df["stacks"] = stacks.flatten()
-         df["superficie"] = surf.flatten()
-         df["tiempo_acc"] = acc.flatten()
-
-      return df
-
-   def create_model(self, pedido: np.ndarray) -> pd.DataFrame:
+   def create_model(self, order: np.ndarray) -> pd.DataFrame:
 
       """ Possible locations for the items in an order. """
 
-      pedido = np.array(pedido)
-      ks, mks = np.unique(pedido, return_counts=True)
+      order = np.array(order)
+      ks, mks = np.unique(order, return_counts=True)
 
       stacks = self.count_stacks()
       nf_stacks = self.locate_stacks(full=False)
@@ -326,7 +355,9 @@ class Storage():
 
       return model
 
-   def add_solution(self, sol: np.ndarray, model: pd.DataFrame):
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+   def add_solution(self, sol: np.ndarray, model: pd.DataFrame) -> Storage:
 
       A: Storage = deepcopy(self)
       locs = [l[0] for l in np.argwhere(sol != 0)]
@@ -361,8 +392,8 @@ def find_positions(
    stack_check = (sol < model["slots"]).values
 
    # check location capacity constraints
-   A = A.add_solution(sol, model)
-   free = [n[0] for n in np.argwhere(A.sn[n] - A.calc_surface().sum(axis=1) >= A.sk[k])]
+   B = A.add_solution(sol, model)
+   free = [n[0] for n in np.argwhere(B.sn[n] - B.calc_surface().sum(axis=1) >= A.sk[k])]
    capacity_check = model["location"].isin(free).values
    
    # check duplicity (equivalent solutions) constraints
@@ -379,7 +410,7 @@ def find_positions(
    return product_check & stack_check & capacity_check & duplicity_check
 
 def create_initial_solution(
-      pedido: np.ndarray,
+      order: np.ndarray,
       model: pd.DataFrame, 
       A: Storage,
       random_state: int = 0
@@ -390,7 +421,7 @@ def create_initial_solution(
    rng = np.random.default_rng(seed=random_state)
    sol = np.zeros(len(model), dtype=int)
 
-   for k in pedido:
+   for k in order:
       vpos = find_positions(k, sol, model, A)
       # TODO change to a more reasonable heuristic
       cpos = rng.choice(np.argwhere(vpos))
@@ -424,6 +455,7 @@ def neighbor_generator(
       lsol = sol.copy()
       lsol[l] -= 1
       npos = find_positions(k, sol, model, A)
+      npos[l] = False
       choices = np.argwhere(npos)
       for c in choices:
          neigh = lsol.copy()
@@ -445,22 +477,23 @@ def local_search(
 
    c_sol = sol
    c_scost = A.calc_solution_surface(c_sol, model)
-   c_tcost = 0#A.calc_solution_access(c_sol)
+   c_tcost = A.calc_solution_access(c_sol, model)
    c_cost = _cost_func(c_scost, c_tcost)
 
    for it in range(max_iterations):
 
       # greedy approach
       for i, neigh in enumerate(neighbor_generator(c_sol, model, A)):
-         
+
          n_scost = A.calc_solution_surface(neigh, model)
-         n_tcost = 0#A.calc_solution_access(neigh)
+         n_tcost = A.calc_solution_access(neigh,  model)
          n_cost = _cost_func(n_scost, n_tcost)
 
          if n_cost < c_cost or i == max_neighbors:
             c_sol = neigh
             c_cost = n_cost
-            print(n_scost, n_tcost)
+
+   return c_sol
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
@@ -476,7 +509,7 @@ if __name__ == "__main__":
    # añade las ubicaciones
    for n in range(N):
       sn = 2 if n%2==0 else 4
-      tn = 1 if n%2==1 else 1.5
+      tn = np.arange(n).sum() + 1
       ln = True if n > N/2 else False
       A.add_location(sn=sn, tn=tn, ln=ln)
 
@@ -493,19 +526,53 @@ if __name__ == "__main__":
    for item, loc in zip(objs, locs):
       A.add_item(loc, item)
 
-   pedido = [1,1,0,1,2]
+   order = [1,1,0,1,2,1,0]
 
-   print("Storage:\n", A)
-   print("\nStorage_status:\n", A.location_info())
-   print("\nPedido:\n", pedido)
-   model = A.create_model(pedido)
-   print("\nModelo:\n", model)
-   sol = create_initial_solution(pedido, model, A, random_state=0)
+   print("\n" + "#"*60)
+
+   print("\nStorage:\n", A)
+   print("\nStorage summary:\n", A.location_summary())
+   print("\nProduct summary:\n", A.product_summary())
+
+   print("\nStor. Surface cost:", A.calc_surface().sum())
+   print("Stor. Access cost:", A.calc_access().sum())
+
+   print("\n" + "#"*60)
+
+   print("\nOrder:\n", order)
+   model = A.create_model(order)
+   print("\nModel:\n", model)
+   sol = create_initial_solution(order, model, A, random_state=1)
+
+   print("\n" + "#"*60)
+
    print("\nRandom initial solution (array):\n", sol)
    print("\nRandom initial solution (human):\n", display_solution(sol, model))
-   print("\nStorage cost: ", A.calc_solution_surface(sol, model))
-   #print("Access cost: ", A.calc_solution_access(sol, model))
-   #local_search(sol, model, A)
+   
+   print("\nStor. Surface cost:", A.calc_surface().sum())
+   print("Diff. Surface cost:", A.calc_solution_surface(sol, model))
+   print(" New  Surface cost:", A.add_solution(sol, model).calc_surface().sum())
+
+   print("\nStor. Access cost:", A.calc_access().sum())
+   print("Diff. Access cost:", A.calc_solution_access(sol, model))
+   print(" New  Access cost:", A.add_solution(sol, model).calc_access().sum())
+   
+   print("\n" + "#"*60)
+
+   bsol = local_search(sol, model, A)
+
+   print("\n" + "#"*60)
+
+   print("\nOptimized solution (array):\n", bsol)
+   print("\nOptimized solution (human):\n", display_solution(bsol, model))
+
+   print("\nStor. Surface cost:", A.calc_surface().sum())
+   print("Diff. Surface cost:", A.calc_solution_surface(bsol, model))
+   print(" New  Surface cost:", A.add_solution(bsol, model).calc_surface().sum())
+
+   print("\nStor. Access cost:", A.calc_access().sum())
+   print("Diff. Access cost:", A.calc_solution_access(bsol, model))
+   print(" New  Access cost:", A.add_solution(bsol, model).calc_access().sum())
    
    # TODO solve local search bugs
    # TODO finish local search
